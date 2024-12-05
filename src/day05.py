5aea6b2b23e5098bb4172ab0063f4375acffe9fd
Joseph Ramaswami <jramaswami@gmail.com>
2024-12-05 06:06:55 -0500

    Solution to second part of day 5.

--- src/day05.py
+++ src/day05.py
@@ -68,13 +68,58 @@ def test_solve1():
     assert solve1(edges, updates) == 143
 
 
+def sort_update(edges, update):
+    "Topological sorting"
+    # Filter to just the edges containing only nodes in current update
+    graph = collections.defaultdict(list)
+    indegree = collections.defaultdict(int)
+    for u, v in edges:
+        if u in update and v in update:
+            graph[u].append(v)
+            indegree[v] += 1
+
+    S = collections.deque()
+    for u in update:
+        if indegree[u] == 0:
+            S.append(u)
+
+    topologically_sorted_update = []
+    while S:
+        u = S.popleft()
+        topologically_sorted_update.append(u)
+        for v in graph[u]:
+            indegree[v] -= 1
+            if indegree[v] == 0:
+                S.append(v)
+    return tuple(topologically_sorted_update)
+
+
+def solve2(edges, updates):
+    soln = 0
+    for update in updates:
+        if not is_in_order(edges, update):
+            update0 = sort_update(edges, update)
+            i = len(update0) // 2
+            middle_value = update0[i]
+            soln += middle_value
+    return soln
+
+
+def test_solve2():
+    edges, updates = parse_input(os.path.join('data', 'test05a.txt'))
+    assert solve2(edges, updates) == 123
+
+
 def main():
     "Main program"
-    # edges, updates = parse_input(os.path.join('data', 'test05a.txt'))
     edges, updates = parse_input(os.path.join('data', 'input05.txt'))
     soln = solve1(edges, updates)
     print('Part 1:', soln)
     assert soln == 5964
+
+    soln = solve2(edges, updates)
+    print('Part 2:', soln)
+    assert soln == 4719
 
     pyperclip.copy(soln)
 
