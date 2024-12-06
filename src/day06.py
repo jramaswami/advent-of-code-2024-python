9dd316e91222a348a95faac4bc5a5a6ab6fc070f
Joseph Ramaswami <jramaswami@gmail.com>
2024-12-06 05:06:19 -0500

    Solution to first part of day 6.

---
+++ src/day06.py
@@ -0,0 +1,68 @@
+import dataclasses
+import os
+import sys
+
+import pyperclip
+
+
+@dataclasses.dataclass(frozen=True)
+class Vector:
+    r: int
+    c: int
+
+    def __add__(self, other):
+        return Vector(self.r + other.r, self.c + other.c)
+
+
+def parse_input(filepath):
+    with open(filepath, 'r') as infile:
+        grid = [list(line.strip()) for line in infile if line.strip()]
+    return grid
+
+
+def inbounds(grid, posn):
+    return posn.r >= 0 and posn.r < len(grid) and posn.c >= 0 and posn.c < len(grid[posn.r])
+
+
+def solve1(grid):
+    # Find guard position.
+    guard = None
+    for r, row in enumerate(grid):
+        for c, x in enumerate(row):
+            if x == '^':
+                guard = Vector(r, c)
+    assert guard
+
+    offsets = (Vector(-1, 0), Vector(0, 1), Vector(1, 0), Vector(0, -1))
+    dirn = 0
+
+    visited = [[0 for _ in row] for row in grid]
+    while inbounds(grid, guard):
+        visited[guard.r][guard.c] = 1
+        guard0 = guard + offsets[dirn]
+        while inbounds(grid, guard0) and grid[guard0.r][guard0.c] == '#':
+            # turn
+            dirn = dirn + 1
+            dirn %= len(offsets)
+            guard0 = guard + offsets[dirn]
+        guard = guard0
+    return sum(sum(row) for row in visited)
+
+
+def test_solve1():
+    grid = parse_input(os.path.join('data', 'test06a.txt'))
+    assert solve1(grid) == 41
+
+
+def main():
+    "Main program"
+    grid = parse_input(os.path.join('data', 'input06.txt'))
+    soln = solve1(grid)
+    print('Part 1:', soln)
+    assert soln == 4711
+
+    pyperclip.copy(soln)
+
+
+if __name__ == '__main__':
+    main()
