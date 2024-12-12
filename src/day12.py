d461c616342c46d8cba2d141132475066b7d68e0
Joseph Ramaswami <jramaswami@gmail.com>
2024-12-12 07:23:35 -0500

    Solution to first part of day 12.

--- src/day12.py
+++ src/day12.py
@@ -21,50 +21,56 @@ def neighbors(grid, r, c):
         yield r0, c0
 
 
+def compute_perimeter(grid, field):
+    perimeter = 0
+    for r, c in field:
+        for r0, c0 in neighbors(grid, r, c):
+            if not inbounds(grid, r0, c0):
+                perimeter += 1
+            elif grid[r0][c0] != grid[r][c]:
+                perimeter += 1
+    return perimeter
+
+
+def find_field(grid, visited, origin_r, origin_c):
+    # BFS
+    queue = collections.deque()
+    queue.append((origin_r, origin_c))
+    visited[origin_r][origin_c] = True
+    field_label = grid[origin_r][origin_c]
+    field = set()
+    while queue:
+        r, c = queue.popleft()
+        field.add((r, c))
+        for r0, c0 in neighbors(grid, r, c):
+            if inbounds(grid, r0, c0) and not visited[r0][c0] and grid[r0][c0] == field_label:
+                queue.append((r0, c0))
+                visited[r0][c0] = True
+    return field
+
+
 def solve1(grid):
     soln = 0
     visited = [[False for _ in row] for row in grid]
     for r, row in enumerate(grid):
         for c, val in enumerate(row):
             if not visited[r][c]:
-                # BFS
-                queue = collections.deque()
-                queue.append((r, c))
-                visited[r][c] = True
-                field_label = grid[r][c]
-                perimeter = 0
-                area = 0
-                while queue:
-                    r, c = queue.popleft()
-                    area += 1
-                    for r0, c0 in neighbors(grid, r, c):
-                        if not inbounds(grid, r0, c0):
-                            perimeter += 1
-                            print('perimeter', r, c, '->', r0, c0)
-                        elif grid[r0][c0] != field_label:
-                            perimeter += 1
-                            print('perimeter', r, c, '->', r0, c0)
-                        elif not visited[r0][c0]:
-                            queue.append((r0, c0))
-                            visited[r0][c0] = True
-                print(field_label, area, perimeter)
+                field = find_field(grid, visited, r, c)
+                area = len(field)
+                perimeter = compute_perimeter(grid, field)
                 soln += (area * perimeter)
     return soln
 
 
 def test_solve1():
-    # grid = parse_input(os.path.join('data', 'test12a.txt'))
-    # assert solve1(grid) == 772
+    grid = parse_input(os.path.join('data', 'test12a.txt'))
+    assert solve1(grid) == 772
 
-    # grid = parse_input(os.path.join('data', 'test12b.txt'))
-    # assert solve1(grid) == 1930
+    grid = parse_input(os.path.join('data', 'test12b.txt'))
+    assert solve1(grid) == 1930
 
-    # grid = parse_input(os.path.join('data', 'test12c.txt'))
-    # assert solve1(grid) == 314
-
-    grid = parse_input(os.path.join('data', 'test12d.txt'))
-    assert solve1(grid) == 300
-
+    grid = parse_input(os.path.join('data', 'test12c.txt'))
+    assert solve1(grid) == 314
 
 
 def main():
@@ -72,7 +78,7 @@ def main():
     grid = parse_input(os.path.join('data', 'input12.txt'))
     soln = solve1(grid)
     print('Part 1:', soln)
-    # 1466700 is too low
+    assert soln == 1467094
     pyperclip.copy(soln)
 
 
