6d5e262d9714eec1243f0d5c852a4f48f1c4072a
Joseph Ramaswami <jramaswami@gmail.com>
2024-12-15 14:52:26 -0500

    Solution to second part of day 15.

--- src/day15.py
+++ src/day15.py
@@ -9,6 +9,9 @@ class Direction:
         self.row = r
         self.column = c
 
+    def __eq__(self, other):
+        return self.row == other.row and self.column == other.column
+
     def __repr__(self):
         return f'Direction({self.row}, {self.column})'
 
@@ -18,14 +21,123 @@ class Box1:
         self.row = r
         self.column = c
 
+    def can_move(self, direction, items):
+        r0, c0 = self.row + direction.row, self.column + direction.column
+        result = True
+        for item in items:
+            if item.collides(r0, c0):
+                result = result and item.can_move(direction, items)
+        return result
+
+    def move(self, direction, items):
+        r0, c0 = self.row + direction.row, self.column + direction.column
+        for item in items:
+            if item.collides(r0, c0):
+                item.move(direction, items)
+        assert not any(item.collides(r0, c0) for item in items)
+        self.row = r0
+        self.column = c0
+
+    def collides(self, r, c):
+        return r == self.row and c == self.column
+
     def __repr__(self):
         return f'Box1({self.row}, {self.column})'
+
+
+class Box2:
+    def __init__(self, r, c):
+        self.row = r
+        self.column1 = c
+        self.column2 = c + 1
+
+    def can_move(self, direction, items):
+        # print(f'Box2 can_move({self}, {direction}, ...)')
+        if direction == Direction(r=0, c=1):
+            # Right
+            r0, c0 = self.row + direction.row, self.column2 + direction.column
+            result = True
+            for item in items:
+                if item.collides(r0, c0):
+                    result = result and item.can_move(direction, items)
+            return result
+        elif direction == Direction(r=0, c=-1):
+            # Left
+            r0, c0 = self.row + direction.row, self.column1 + direction.column
+            result = True
+            for item in items:
+                if item.collides(r0, c0):
+                    result = result and item.can_move(direction, items)
+            return result
+        elif direction == Direction(r=1, c=0):
+            r0, c0, c1 = self.row + direction.row, self.column1 + direction.column, self.column2 + direction.column
+            result = True
+            for item in items:
+                if item.collides(r0, c0) or item.collides(r0, c1):
+                    result = result and item.can_move(direction, items)
+            return result
+        elif direction == Direction(r=-1, c=0):
+            r0, c0, c1 = self.row + direction.row, self.column1 + direction.column, self.column2 + direction.column
+            result = True
+            for item in items:
+                if item.collides(r0, c0) or item.collides(r0, c1):
+                    result = result and item.can_move(direction, items)
+            return result
+
+    def move(self, direction, items):
+        if direction == Direction(r=0, c=1):
+            # Right
+            r0, c0 = self.row + direction.row, self.column2 + direction.column
+            for item in items:
+                if item.collides(r0, c0):
+                    item.move(direction, items)
+            assert not any(item.collides(r0, c0) for item in items)
+            self.column1 += 1
+            self.column2 += 1
+        elif direction == Direction(r=0, c=-1):
+            # Left
+            r0, c0 = self.row + direction.row, self.column1 + direction.column
+            for item in items:
+                if item.collides(r0, c0):
+                    item.move(direction, items)
+            assert not any(item.collides(r0, c0) for item in items)
+            self.column1 -= 1
+            self.column2 -= 1
+        elif direction == Direction(r=1, c=0):
+            r0, c0, c1 = self.row + direction.row, self.column1 + direction.column, self.column2 + direction.column
+            for item in items:
+                if item.collides(r0, c0) or item.collides(r0, c1):
+                    item.move(direction, items)
+            assert not any(item.collides(r0, c0) or item.collides(r0, c1) for item in items)
+            self.row += 1
+        elif direction == Direction(r=-1, c=0):
+            r0, c0, c1 = self.row + direction.row, self.column1 + direction.column, self.column2 + direction.column
+            for item in items:
+                if item.collides(r0, c0) or item.collides(r0, c1):
+                    item.move(direction, items)
+            assert not any(item.collides(r0, c0) or item.collides(r0, c1) for item in items)
+            self.row -= 1
+
+    def collides(self, r, c):
+        return r == self.row and c in [self.column1, self.column2]
+
+    def __repr__(self):
+        return f'Box2({self.row}, [{self.column1}, {self.column2}])'
 
 
 class Wall:
     def __init__(self, r, c):
         self.row = r
         self.column = c
+
+    def can_move(self, direction, items):
+        return False
+
+    def move(self, direction, items):
+        raise ValueError(f'Cannot move a wall {self}')
+
+    def collides(self, r, c):
+        return r == self.row and c == self.column
 
     def __repr__(self):
         return f'Wall({self.row}, {self.column})'
@@ -36,31 +148,28 @@ class Robot:
         self.row = r
         self.column = c
 
+    def can_move(self, direction, items):
+        r0, c0 = self.row + direction.row, self.column + direction.column
+        result = True
+        for item in items:
+            if item.collides(r0, c0):
+                result = result and item.can_move(direction, items)
+        return result
+
+    def move(self, direction, items):
+        r0, c0 = self.row + direction.row, self.column + direction.column
+        for item in items:
+            if item.collides(r0, c0):
+                item.move(direction, items)
+        assert not any(item.collides(r0, c0) for item in items)
+        self.row = r0
+        self.column = c0
+
+    def collides(self, r, c):
+        return r == self.row and c == self.column
+
     def __repr__(self):
         return f'Robot({self.row}, {self.column})'
-
-
-def can_move(item, direction, items):
-    if isinstance(item, Wall):
-        return False
-
-    r0, c0 = item.row + direction.row, item.column + direction.column
-    result = True
-    for item0 in items:
-        if item0.row == r0 and item0.column == c0:
-            result = result and can_move(item0, direction, items)
-    return result
-
-
-def move(item, direction, items):
-    assert not isinstance(item, Wall)
-    r0, c0 = item.row + direction.row, item.column + direction.column
-    for item0 in items:
-        if item0.row == r0 and item0.column == c0:
-            move(item0, direction, items)
-    assert not any(item.row == r0 and item.column == c0 for item in items)
-    item.row = r0
-    item.column = c0
 
 
 def parse_input(filepath):
@@ -81,12 +190,15 @@ def parse_input(filepath):
 
 
 def make_grid(items, robot, N, M):
-    grid0 = [['.' for _ in range(M)] for _ in range(M)]
+    grid0 = [['.' for _ in range(M)] for _ in range(N)]
     for item in items:
         if isinstance(item, Wall):
             grid0[item.row][item.column] = '#'
         elif isinstance(item, Box1):
             grid0[item.row][item.column] = 'O'
+        elif isinstance(item, Box2):
+            grid0[item.row][item.column1] = '['
+            grid0[item.row][item.column2] = ']'
     grid0[robot.row][robot.column] = '@'
     return '\n'.join(''.join(row) for row in grid0)
 
@@ -104,6 +216,8 @@ def solve1(grid, instructions):
                 items.append(Wall(r, c))
             elif val == 'O':
                 items.append(Box1(r, c))
+            elif val == '[':
+                items.append(Box2(r, c))
 
     assert robot
     assert items
@@ -114,18 +228,20 @@ def solve1(grid, instructions):
         'v': Direction(1, 0)
     }
 
-    print(make_grid(items, robot, N, M))
+    # print(make_grid(items, robot, N, M))
     for i, instruction in enumerate(instructions):
-        print(i, instruction, robot)
+        # print(i, instruction, robot)
         direction = directions[instruction]
-        if can_move(robot, direction, items):
-            move(robot, direction, items)
-        print(make_grid(items, robot, N, M))
+        if robot.can_move(direction, items):
+            robot.move(direction, items)
+        # print(make_grid(items, robot, N, M))
 
     soln = 0
     for item in items:
         if isinstance(item, Box1):
             soln += (100 * item.row) + item.column
+        if isinstance(item, Box2):
+            soln += (100 * item.row) + item.column1
     return soln
 
 
@@ -136,15 +252,49 @@ def test_solve1():
     assert solve1(grid, instructions) == 10092
 
 
+def double_grid(grid):
+    grid0 = []
+    for row in grid:
+        row0 = []
+        for x in row:
+            if x == '#':
+                row0.append('#')
+                row0.append('#')
+            elif x == 'O':
+                row0.append('[')
+                row0.append(']')
+            elif x == '@':
+                row0.append('@')
+                row0.append('.')
+            elif x == '.':
+                row0.append('.')
+                row0.append('.')
+            else:
+                raise ValueError(f'Unrecognized map symbol "{x}"')
+        grid0.append(tuple(row0))
+    return tuple(grid0)
+
+
+def solve2(grid, instructions):
+    grid0 = double_grid(grid)
+    return solve1(grid0, instructions)
+
+
+def test_solve2():
+    grid, instructions = parse_input(os.path.join('data', 'test15b.txt'))
+    assert solve2(grid, instructions) == 9021
+
+
 def main():
     "Main program"
-    # Configure logging
     grid, instructions = parse_input(os.path.join('data', 'input15.txt'))
-    # grid, instructions = parse_input(os.path.join('data', 'test15a.txt'))
     soln = solve1(grid, instructions)
     print('Part 1:', soln)
     assert soln == 1509863
-    # pyperclip.copy(soln)
+    soln = solve2(grid, instructions)
+    print('Part 2:', soln)
+    pyperclip.copy(soln)
+    assert soln == 1548815
 
 
 if __name__ == '__main__':
