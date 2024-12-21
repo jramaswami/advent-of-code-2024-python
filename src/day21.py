3d337b4b0d6a869235908bb32dfd173fa5aec229
Joseph Ramaswami <jramaswami@gmail.com>
2024-12-21 08:38:19 -0500

    WA

--- src/day21.py
+++ src/day21.py
@@ -1,9 +1,7 @@
 import collections
 import dataclasses
-import math
 import os
 import sys
-
 
 
 @dataclasses.dataclass(frozen = True)
@@ -11,64 +9,102 @@ class Vector:
     row :int
     col :int
 
-    def __add__(self, other):
-        return Vector(self.row + other.row, self.col + other.col)
+    def manhattan_distance(self, other):
+        return abs(self.row - other.row) + abs(self.col - other.col)
 
+
+################################### KEYPADS ####################################
 
 NUMBER_PAD = tuple(tuple(row) for row in '789\n456\n123\n 0A'.split('\n'))
 DIRECTION_PAD = tuple(tuple(row) for row in ' ^A\n<v>'.split('\n'))
-OFFSETS = {
-    '^': Vector(-1, 0),
-    'v': Vector(1, 0),
-    '<': Vector(0, -1),
-    '>': Vector(0, 1)
-}
+
+NP_LOCATIONS = dict()
+for r, row in enumerate(NUMBER_PAD):
+    for c, val in enumerate(row):
+        if val != ' ':
+            NP_LOCATIONS[val] = Vector(r, c)
+
+DP_LOCATIONS = dict()
+for r, row in enumerate(DIRECTION_PAD):
+    for c, val in enumerate(row):
+        if val != ' ':
+            DP_LOCATIONS[val] = Vector(r, c)
+
+################################################################################
 
 
-def inbounds(grid, p):
-    return p.row >= 0 and p.row < len(grid) and p.col >= 0 and p.col < len(grid[p.row])
+def parse_input(filepath):
+    with open(filepath, 'r') as infile:
+        data = tuple(line.strip() for line in infile if line.strip())
+    return data
 
 
-def neighbors(grid, p):
-    for _, o in OFFSETS.items():
-        p0 = p + o
-        if inbounds(grid, p0) and grid[p0.row][p0.col] != ' ':
-            yield p0
+def parse_expected(filepath):
+    """Parse result file and return a dictionary where the key
+    is the code and the value is the length of the expected sequence
+    after three translations.
+    """
+    data = {}
+    with open(filepath, 'r') as infile:
+        for line in infile:
+            line = line.strip()
+            if line:
+                code, sequence = line.split(': ')
+                data[code] = len(sequence.strip())
+    return data
 
 
-def all_paths(grid):
+def translate(code, keypad_locations):
+    # Starts at 'A' button
+    translation = []
+    curr_key = keypad_locations['A']
+    curr_key_val = 'A'
+    for next_key_val in code:
+        next_key = keypad_locations[next_key_val]
+        dr = next_key.row - curr_key.row
+        dc = next_key.col - curr_key.col
+        extension = ''
+        if dc < 0:
+            extension += ('<'*abs(dc))
+        elif dc > 0:
+            extension += ('>'*abs(dc))
+        if dr < 0:
+            extension += ('^'*abs(dr))
+        elif dr > 0:
+            extension += ('v'*abs(dr))
+        print('curr', curr_key, curr_key_val, 'next', next_key, next_key_val, f'{dr=} {dc=}', extension)
+        translation.append(extension)
+        curr_key = next_key
+        curr_key_val = next_key_val
+        # Press curr_key
+        translation.append('A')
+    return ''.join(translation)
 
-    def bfs(origin):
-        dist = collections.defaultdict(lambda: math.inf)
-        queue = collections.deque()
-        dist[origin] = 0
-        queue.append(origin)
-        while queue:
-            p = queue.popleft()
-            for p0 in neighbors(grid, p):
-                if dist[p] + 1 < dist[p0]:
-                    dist[p0] = dist[p] + 1
-                    queue.append(p0)
-        return dist
 
-    paths = dict()
-    for r, row in enumerate(grid):
-        for c, val in enumerate(row):
-            if val != ' ':
-                origin = Vector(r, c)
-                x = grid[origin.row][origin.col]
-                dist = bfs(origin)
-                paths[x] = dict()
-                for p, d in dist.items():
-                    y = grid[p.row][p.col]
-                    paths[x][y] = d
-    return paths
+def shortest_sequence_length(original_code, translations):
+    code = original_code
+    keypad_locations = NP_LOCATIONS
+    print(code, len(code))
+    for _ in range(translations):
+        code = translate(code, keypad_locations)
+        print(code, len(code))
+        keypad_locations = DP_LOCATIONS
+    return len(code)
+
+
+def test_shortest_sequence_length():
+    codes = parse_input(os.path.join('data', 'test21a.txt'))
+    expected = parse_expected(os.path.join('data', 'expected21a.txt'))
+    for code in codes:
+        print('*' * 40, code, '*' * 40)
+        assert shortest_sequence_length(code, 3) == expected[code]
+
+
+def solve1(original_code, translations=3):
+    pass
 
 def main():
     "Main program"
-    numeric_paths = all_paths(NUMBER_PAD)
-    for k in numeric_paths:
-        print(k, '->', numeric_paths[k])
 
 
 if __name__ == '__main__':
