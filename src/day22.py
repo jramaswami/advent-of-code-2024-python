b0a31ca5c88a2ab8329c568929650d6870bac29a
Joseph Ramaswami <jramaswami@gmail.com>
2024-12-22 08:07:09 -0500

    Solution to both parts of day 22.

--- src/day22.py
+++ src/day22.py
@@ -1,3 +1,5 @@
+import collections
+import itertools
 import os
 import sys
 
@@ -50,12 +52,63 @@ def test_solve1():
     assert solve1(numbers) == 37327623
 
 
+class Record:
+    def __init__(self, secret):
+        self.secret = secret
+        self.price = secret % 10
+        self.change = None
+        self.window = None
+
+    def __str__(self):
+        return f"{self.secret} {self.price} {self.change} {self.window}"
+
+
+class Monkey:
+    def __init__(self, secret):
+        self.records = []
+        self.records.append(Record(secret))
+        for _ in range(2000):
+            secret = evolve(secret)
+            self.records.append(Record(secret))
+
+        for i, _ in enumerate(self.records[1:], start=1):
+            self.records[i].change = self.records[i].price - self.records[i-1].price
+
+        for i, _ in enumerate(self.records[4:], start=4):
+            self.records[i].window = tuple(x.change for x in self.records[i-3:i+1])
+
+        # Cache
+        self.cache = dict()
+        for record in self.records:
+            if record.window not in self.cache:
+                self.cache[record.window] = record.price
+
+    def bananas_for(self, target_window):
+        return self.cache.get(target_window, 0)
+
+
+def solve2(numbers):
+    monkeys = [Monkey(n) for n in numbers]
+    possible = set()
+    for monkey in monkeys:
+        for record in monkey.records:
+            if record.window:
+                possible.add(record.window)
+    soln = 0
+    for target in possible:
+        soln = max(soln, sum(monkey.bananas_for(target) for monkey in monkeys))
+    return soln
+
+
 def main():
     "Main program"
     numbers = parse_input(os.path.join('data', 'input22.txt'))
     soln = solve1(numbers)
     print('Part 1:', soln)
     assert soln == 16999668565
+    soln = solve2(numbers)
+    print('Part 2:', soln)
+    assert soln == 1898
     pyperclip.copy(soln)
 
 
