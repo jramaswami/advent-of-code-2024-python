405521e7a51196fdd0cd72295d9a769e30d20e83
Joseph Ramaswami <jramaswami@gmail.com>
2024-12-23 08:32:35 -0500

    Solution to second part of day 23.

--- src/day23.py
+++ src/day23.py
@@ -7,14 +7,14 @@ import pyperclip
 
 
 def parse_input(filepath):
-    graph = collections.defaultdict(list)
+    graph = collections.defaultdict(set)
     with open(filepath, 'r') as infile:
         for line in infile:
             line = line.strip()
             if line:
                 u, v = (x.strip() for x in line.split('-'))
-                graph[u].append(v)
-                graph[v].append(u)
+                graph[u].add(v)
+                graph[v].add(u)
     return graph
 
 
@@ -33,13 +33,42 @@ def test_solve1():
     assert solve1(graph) == 7
 
 
+def solve2(graph):
+    """Find the maximal clique"""
+    max_clique = None
+    max_clique_length = 0
+    nodes = list(graph.keys())
+    visited = set()
+    for root in nodes:
+        if root not in visited:
+            # Try to find a maximal clique from here
+            clique = set()
+            clique.add(root)
+            for node in nodes:
+                if graph[node].issuperset(clique):
+                    clique.add(node)
+            visited.update(clique)
+            if len(clique) > max_clique_length:
+                max_clique = clique
+                max_clique_length = len(clique)
+    return ','.join(sorted(max_clique))
+
+
+def test_solve2():
+    graph = parse_input(os.path.join('data', 'test23a.txt'))
+    assert solve2(graph) == 'co,de,ka,ta'
+
+
 def main():
     """Main program"""
     graph = parse_input(os.path.join('data', 'input23.txt'))
     soln = solve1(graph)
     print('Part 1:', soln)
-    pyperclip.copy(soln)
     assert soln == 1370
+    soln = solve2(graph)
+    print('Part 2:', soln)
+    pyperclip.copy(soln)
+    assert soln == 'am,au,be,cm,fo,ha,hh,im,nt,os,qz,rr,so'
 
 
 if __name__ == '__main__':
